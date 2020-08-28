bcdba760de0bd125b4e69742e9380072a9e81a77
opilane <airi@rosimannus.ee>
2020-08-28 15:34:01 +0300

    jSONLint harjutus, 28.08

---
+++ src/JsonHarjutus.java
@@ -0,0 +1,27 @@
+public class JsonHarjutus {
+/*  jsonlint.com (The jSON Validator -is tegime)
+    {
+        "Client": [{
+        "Name": "Airi Rosimannus",
+                "Age": "32",
+                "Address": "Salu 17, Rapla",
+                "Car": true,
+                "Car information": [{
+            "Car number": "111 AAA",
+                    "Car brand": "Jaguar x pace"
+        }]
+
+    },
+        {
+            "Name": "Mirko Põder",
+                "Age": "30",
+                "Address": "Tallinn",
+                "Car": true,
+                "Car information": [{
+            "Car number": "111 BMW",
+                    "Car brand": "BMW"
+        }]
+
+        }
+   	]
+    } */
